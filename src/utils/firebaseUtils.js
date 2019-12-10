e7d11e935a3c5273110b15b5c4699296350721c5
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-12-10 17:57:49 -0300

    instalação do firebase

---
+++ src/utils/firebaseUtils.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const config = {
+    
+    apiKey: "AIzaSyAtVS4LihVHs8DDntUHiUz6Sp0-s2coEZ0",
+    authDomain: "burger-queen-aff55.firebaseapp.com",
+    databaseURL: "https://burger-queen-aff55.firebaseio.com",
+    projectId: "burger-queen-aff55",
+    storageBucket: "burger-queen-aff55.appspot.com",
+    messagingSenderId: "653856621681",
+    appId: "1:653856621681:web:96a346505f04225f7847d0",
+    measurementId: "G-DZ76L7SWZY"
+
+};
+   
+
+export const firebaseImpl = firebase.initializeApp(config);
+export const firebaseDatabase = firebase.database();
