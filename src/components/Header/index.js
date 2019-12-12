e973aab0a0e2e7c1aa0a29db62d040235642e40e
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-12-12 19:16:07 -0300

    link com firebase buscando menu no console.log

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles.css';
-
+import logo from './logo_burger_queen.png';
 
 const Header = () => (
     <header id="main-header"> 
      BURGER QUEEN
+     <img src={logo} alt='Logo Burger Queen' />
     </header>
+    
 
 );
 
 export default Header; 
-
-//<img src=""/>
--- src/pages/order.js/Order.js
+++ src/pages/order.js/Order.js
@@ -1,119 +1,102 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header/index.js'; 
-import Info from '../../components/Info/index.js' 
 
 import firebase from '../../utils/firebaseUtils';
 import './styles.css';
 
+const AddClientInfo = () => {
+    const [client, setClient] = useState('');
+    const [table, setTable] = useState('');
+    const [pedidos, setPedidos] = useState('');
+    const [total, setTotal] = useState('');
 
-
-firebase.firestore().collection("foodsBreakfast").add({
-  itens: [
-    {
-      name: "Café americano",
-      price: 5
-    },
-    {
-      name: "Café com leite",
-      price: 7
-    },
-    {
-      name: "Misto Quente",
-      price: 10
-    },
-    {
-      name: "Suco natural de fruta",
-      price: 7
+    function onSubmit(e) {
+        e.preventDefault()
+        
+        firebase
+            .firestore()
+            .collection('infoClient')
+            .add({
+                client,
+                table,
+                pedidos,
+                total,
+            })
+            .then(()=>{
+               setTable('')
+               setClient('')
+               setPedidos('')
+               setTotal('')
+                
+            }) 
     }
-  ]
-});
-
-firebase.firestore().collection('burgers').add({
-itens: [
-  {
-    name: "Hambúrguer simples bovino",
-    price: 10,
-  },
-  {
-    name: "Hambúrguer duplo bovino",
-    price: 15
-  },
-  {
-    name: "Hambúrguer simples frango",
-    price: 10,
-  },
-  {
-    name: "Hambúrguer duplo frango",
-    price: 15
-  },{
-    name: "Hambúrguer simples vegetariano",
-    price: 10,
-  },
-  {
-    name: "Hambúrguer duplo vegetariano",
-    price: 10
-  },
-]
-
-});
-
-firebase.firestore().collection('sideDishes').add({
-  itens: [
-    {
-      name: "Batatas fritas",
-      price: 5
-    },
-    {
-      name: "Anéis de cebola",
-      price: 5
-    }
-  ]
-});
-
-firebase.firestore().collection('drinks').add({
-  itens: [
-    {
-      name: "Água 500ml",
-      price: 5
-    },
-    {
-      name: "Água 750ml",
-      price: 7
-    },
-    {
-      name: "Refrigerante 500ml",
-      price: 7
-    },
-    {
-      name: "Refrigerante 750ml",
-      price: 10
-    }
-  ]
-});
-
-firebase.firestore().collection('extras').add({
-  itens: [
-    {
-      name: "Queijo",
-      price: 1
-    },
-    {
-      name: "Ovo",
-      price: 1
-    }
-  ]
-});
   
+  return (
+    <div>
+      <label>
+        <strong>NOME DO CLIENTE</strong>
+      </label>
+      <input id='input-name'type="text" value={client} onChange={e => setClient(e.currentTarget.value)}/>
+      <label>
+        <strong>NÚMERO DA MESA</strong>
+      </label>
+      <input id='input-number'type="number" value={table} onChange={e => setTable(e.currentTarget.value)}/>
+      <button id='btn-food' onClick={onSubmit}><strong>Enviar Pedidos</strong></button>
+    </div>
+   
+  );
+};
+
+function MenuCoffee(){
+
+  const [itens, setItens] = useState([])
+
+useEffect(() => {
+  const unsubscribe = firebase
+    .firestore()
+    .collection('foodsBreakfast')
+    .onSnapshot((snapshot) => {
+      const products = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setItens(products)
+    })
+    return () => unsubscribe()
+}, [])
+ return itens
+}
+
+
 
 function Order() {
+
+  const itens = MenuCoffee()
   return (
     <div className="App">
       <Header/>
-      <Info/>
-      <h1>Aqui vai rolar o APP</h1>
-      
+      <AddClientInfo/>
+      <h1>Café</h1>
+      <ol>
+        {itens.map((itens) => 
+          <div key={itens.id}>
+            <div className='itens-entry'> 
+              {itens.name}
+              <code className='itens'>{itens.price}reais</code>
+            </div>
+          </div>
+        )}
+      </ol>
+      <h1>Hambúrguer</h1>
+      <p></p>
+      <h1>Acompanhamentos</h1>
+      <p></p>
+      <h1>Bebidas</h1>
+      <p></p>
+      <h1>Extras</h1>
+      <p></p>     
     </div>
   );
 }
 
-export default Order;
+export default Order
