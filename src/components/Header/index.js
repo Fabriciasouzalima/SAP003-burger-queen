dce89099a16b622be33c9756013b6609f10bcdcf
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-12-28 23:17:11 -0300

    atualização na rota, implementada porém não segue o caminho

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Link } from "react-router-dom";
+import {BrowserRouter, Link } from "react-router-dom";
 import './styles.css';
-import App from '../../routes.js'
 
 import Logo from './logo_burger_queen.png';
 
@@ -10,10 +9,13 @@ const Header = () => (
   <header className="main-header">
     <img width={"50px"} src={Logo} alt="Logo Burger Queen" />
     BURGER QUEEN
-    <App/> 
     <BrowserRouter>
-    <Link to="/kitchen" className='links'>Cozinha</Link>
-    <Link to="/Order" className='links'>Salão</Link>
+      <Link to="/kitchen" className="links">
+        Cozinha
+      </Link>
+      <Link to="/Order" className="links">
+        Salão
+      </Link>
     </BrowserRouter>
   </header>
 );
