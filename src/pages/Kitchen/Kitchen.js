cb4a568c82374f26e39cfe9e48a4f45851c889bd
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-12-31 14:25:37 -0300

    printar informações dos pedidos na tela

--- src/pages/Kitchen/Kitchen.js
+++ src/pages/Kitchen/Kitchen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import ClientOrders from '../../components/AllOrders/allOrders.js'
+
+import "./styles.css"
 
 
 function Kitchen() {
@@ -11,15 +12,16 @@ function Kitchen() {
   return (
     <div>
       <h1>Ordens em Andamento</h1>
-      {existingOrders.map( existingOrders => (
+      {existingOrders.map(existingOrders => (
         <ul>
           <li>
             Cliente: {existingOrders.client}
             Mesa: {existingOrders.table}
-            Total: R$ {existingOrders.total},00
-            {/* pedidos: {existingOrders.pedidos.map()} */}
-            
-
+            Total: R$ {existingOrders.total},00 
+            pedidos:
+            {existingOrders.pedidos.map(
+              products => (products.product.name) 
+            )}     
           </li>
         </ul>
       ))}
